ff75d25607214da876ea81f67665abc668fcc91b
SudilaJayasekara <91514161+SudilaJayasekara@users.noreply.github.com>
2023-02-26 21:38:20 +0530

    Create test.c

---
+++ Nsbm/test.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+int main(void)
+{
+//	int i;
+//	for(i=0;i<10;i++)
+//	{
+//		printf("%d\n",i);	
+//	}	
+	
+//	int n=0;
+//	while(1)
+//	{
+//		if(n==5)
+//			break;
+//		printf("%d\n",n);
+//		n++;
+//	}
+	
+//	int n=0;
+//	while(n<10)
+//	{
+//		if(n==5)
+//		{
+//			n++;
+//			continue;
+//		}
+//		
+//		printf("%d\n",n);
+//		n++;
+//	}
+
+//	int n = 0;
+//	while(1)
+//	{
+//		if(n==10)
+//			break;
+//		
+//		if(n==5)
+//		{
+//			n++;
+//			continue;
+//		}
+//		
+//		printf("%d\n",n);
+//		n++;
+//		
+//	}
+	
+//	int n = 0;
+//	while(n!=10)
+//	{
+//		printf("%d\n",n);
+//		n++;
+//	}
+	
+//	int n=6;
+//	while(n<=5)
+//	{
+//		printf("%d\n",n);
+//		n++;
+//	}
+
+//	int n=6;
+//	do{
+//		printf("%d\n",n);
+//		n++;
+//	}while(n<=5);
+
+//	int n;
+//	while(n!=-1)
+//	{
+//		printf("Enter a number: ");
+//		scanf("%d",&n);
+//	}
+
+//	int n,total=0;
+//	char let;
+//	while(1)
+//	{
+//		printf("Enter a letter: ");
+//		scanf(" %c",&let);
+//		
+//		if(let=='n'||let=='N')
+//			break;
+//			
+//		else if(let=='y'||let=='Y')
+//		{
+//			printf("Enter a number: ");
+//			scanf("%d",&n);
+//			total+=n;
+//		}
+//
+//	}
+//	printf("Total is : %d",total);
+
+//	int n,total=0;
+//	char let;
+//	do{
+//		printf("Enter a letter: ");
+//		scanf(" %c",&let);
+//		
+//		if(let=='n'||let=='N')
+//			break;
+//		
+//		printf("Enter a number: ");
+//		scanf("%d",&n);
+//		
+//		total+=n;
+//		
+//	}while(1);
+//
+//	printf("Total is : %d",total);
+
+	return 0;
+}
